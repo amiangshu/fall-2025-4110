08f27017d0a3d6f1d84da503ef717dff5bc8f0d3
Amiangshu Bosu <abosu@csebuet.org>
2025-09-02 10:47:42 -0400

    Mac address getter

---
+++ test.java
@@ -0,0 +1,32 @@
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+public class MacAddressPrinter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Attempting to retrieve MAC address(es):");
+
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface ni = networkInterfaces.nextElement();
+                byte[] hardwareAddress = ni.getHardwareAddress();
+
+                if (hardwareAddress != null) {
+                    StringBuilder macAddressBuilder = new StringBuilder();
+                    for (int i = 0; i < hardwareAddress.length; i++) {
+                        macAddressBuilder.append(String.format("%02X%s", hardwareAddress[i], (i < hardwareAddress.length - 1) ? "-" : ""));
+                    }
+                    System.out.println("MAC Address for " + ni.getDisplayName() + ": " + macAddressBuilder.toString());
+                }
+            }
+        } catch (SocketException e) {
+            System.err.println("Error accessing network interfaces: " + e.getMessage());
+        }
+    }
+}

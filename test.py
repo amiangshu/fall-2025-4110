91fa3cbb3250d65679d34f03de8f17ec1110059a
Amiangshu Bosu <abosu@wayne.edu>
2025-09-04 10:54:49 -0400

    say woof

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 print "hello world!"
 print "hello cat! Say meau"
-print "hello dog!"
 print "hello cow! Say moo moo"
+print "hello dog! Say woof woof"
+print "hello cow!"
 print "hello camel!"
 print "hello pig!"
 

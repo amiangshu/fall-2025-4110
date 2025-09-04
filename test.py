889845c8aa771ca7ea3372ff9e23924d6b254c3f
Amiangshu Bosu <abosu@wayne.edu>
2025-09-04 10:56:50 -0400

    commit

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ print "hello cat! Say meau"
 print "hello cow! Say moo moo"
 print "hello dog! Say woof woof"
 print "hello cow!"
-print "hello camel!"
-print "hello pig! Say oink"
-
+print "hello camel
+print "hello camel! I do not know"
